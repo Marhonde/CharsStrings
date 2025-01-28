326a5e12df0a3cf0ea878f7887b21a3cd411f986
Marhonde <SemenKuzer@mail.ru>
2025-01-28 15:17:40 +0700

    first commit

--- CharsStrings/Program.cs
+++ CharsStrings/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CharsStrings;
+
+class Program : Python
+{
+    private static bool _check = true;
+
+    private static int _task, _n;
+
+    private static string? _str;
+    
+    public static void Main(string[] args)
+    {
+        while (_check)
+        {
+            _task = int.Parse(input("Задание: "));
+
+            switch (_task)
+            {
+                case 1:
+                    _str = input("Введите строку: ");
+
+                    _n = int.Parse(input("Введите длину новой строки: "));
+
+                    if (_str.Length > _n)
+                        _str = _str.Remove(_n, _str.Length - _n);
+                    else
+                        _str = _str.PadLeft(_n, '.');
+                    
+                    print($"Полученная строка: {_str}");
+                    
+                    break;
+                case 0:
+                    _check = false;
+                    break;
+                default:
+                    print("ошибка");
+                    break;
+            }
+        }
+    }
+}
